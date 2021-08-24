9dd15a76346cdb5d7a697cd03c40a5f04592b1fe
Carlos Eduardo Barbosa de Almeida <carlosedba@outlook.com>
2021-08-23 21:38:56 -0300

    ProdutoRepository criado + inicio ProdutoResource

---
+++ src/main/java/com/dsws/tde1/ProdutoResource.java
@@ -0,0 +1,9 @@
+package com.dsws.tde1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoResource {
+}
