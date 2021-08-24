1ded510a72978c6f27bb1f81923df14b23f7dd16
Heli <helisana.lula@pucpr.edu.br>
2021-08-23 21:19:01 -0300

    primeira commit

---
+++ src/main/java/com/dsws/tde1/DswsTde1Application.java
@@ -0,0 +1,13 @@
+package com.dsws.tde1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DswsTde1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DswsTde1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/dsws/tde1/DswsTde1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsws.tde1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DswsTde1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
