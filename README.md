802d30d5e231bded1589ab35972b03bc62dea519
Heli <helisana.lula@pucpr.edu.br>
2021-08-23 22:22:11 -0300

    ProdutoResource finalizado

---
+++ README.md
@@ -0,0 +1,5 @@
+#TDE 1
+##Integrantes da equipe
+
+- Carlos Eduardo Barbosa de Almeida
+- Helisana dos Santos
--- src/main/java/com/dsws/tde1/ProdutoResource.java
+++ src/main/java/com/dsws/tde1/ProdutoResource.java
@@ -1,15 +1,13 @@
 package com.dsws.tde1;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("/produto")
 public class ProdutoResource {
     @Autowired
     private ProdutoRepository produtoRepository;
@@ -21,6 +19,31 @@ public class ProdutoResource {
 
     @GetMapping("/{id}")
     public Produto consultar(@PathVariable("id") int id) {
+
         return produtoRepository.getById(id);
     }
+
+    @PostMapping
+    public Produto incluir(@RequestBody Produto p) {
+        return produtoRepository.save(p);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable("id") int id, @RequestBody Produto p) {
+        Produto produto = produtoRepository.getById(id);
+
+        produto.setNome(p.getNome());
+        produto.setAnoFabricacao(p.getAnoFabricacao());
+        produto.setEan(p.getEan());
+        produtoRepository.save(produto);
+
+        return produto;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deletar(@PathVariable("id") int id) {
+        produtoRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
 }
