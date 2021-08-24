09fc92b60d7933a7d9fc0214122afe3aff64a361
Carlos Eduardo Barbosa de Almeida <carlosedba@outlook.com>
2021-08-23 21:42:36 -0300

    Adicionado listar e consultar em Produto

--- src/main/java/com/dsws/tde1/ProdutoResource.java
+++ src/main/java/com/dsws/tde1/ProdutoResource.java
@@ -1,9 +1,26 @@
 package com.dsws.tde1;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoResource {
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    @GetMapping
+    public List<Produto> listar() {
+        return produtoRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto consultar(@PathVariable("id") int id) {
+        return produtoRepository.getById(id);
+    }
 }
